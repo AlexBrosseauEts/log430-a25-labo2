5eea96304276128384092bee45a0ecba359b98cf
Alex Brosseau <brosseau.alex@outlook.com>
2025-09-26 21:36:50 -0400

    Update write_order.py

--- src/commands/write_order.py
+++ src/commands/write_order.py
@@ -4,6 +4,7 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 import json
+import os
 from datetime import datetime
 
 from models.product import Product
